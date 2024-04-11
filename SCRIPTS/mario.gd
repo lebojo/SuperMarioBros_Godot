95dd7631409d51d4f2a6b1898688e1e3388c386f
Jordan MPB <contact@jordan-chap.com>
2024-04-11 20:00:45 +0200

    final

--- SCRIPTS/mario.gd
+++ SCRIPTS/mario.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
-const JUMP_VELOCITY = -1300.0
+const JUMP_VELOCITY = -1400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 5000
