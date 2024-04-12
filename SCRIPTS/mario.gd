71ec7857c786289851988cc037bc85d570bef9fd
Jordan MPB <contact@jordan-chap.com>
2024-04-12 20:00:14 +0200

    New inertia but no collision script for Tilemap

--- SCRIPTS/mario.gd
+++ SCRIPTS/mario.gd
@@ -2,12 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
-const JUMP_VELOCITY = -1400.0
+const JUMP_VELOCITY = -1600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 5000
-var acceleration = 0.0
+var acceleration = -SPEED
 var jump_acceleration = 0.0
+
+var old_dir = 0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -39,15 +41,21 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
+		if direction != old_dir:
+			old_dir = direction
 		if not is_on_floor():
-			if acceleration > SPEED * -1:
-				acceleration -= 10
-		elif acceleration < 0:
-			acceleration = 0
-		velocity.x = direction * (SPEED + acceleration)
+			if acceleration > -SPEED:
+				acceleration -= 5
+		if acceleration < 0:
+			acceleration += 20
+		velocity.x = old_dir * (SPEED + acceleration)
 		$Sprite2D.scale.x = direction
 	else:
-		velocity.x = move_toward(velocity.x, 0, (SPEED + acceleration))
+		if acceleration > -SPEED:
+			acceleration -= 50
+		if acceleration < -SPEED:
+			acceleration = -SPEED
+		velocity.x = old_dir * (SPEED + acceleration)
 
 	move_and_slide()
 	if velocity.x != 0 and is_on_floor():
@@ -56,4 +64,10 @@ func _physics_process(delta):
 		if is_on_floor():
 			$anim.stop()
 			$Sprite2D.frame = 0
-
+	var lst_clsn = get_last_slide_collision()
+	if lst_clsn and lst_clsn.get_collider() is TileMap:
+		var tm = lst_clsn.get_collider()
+		var tm_pos = tm.local_to_map(lst_clsn.get_position())
+		var cell = tm.get_cellv(tm_pos)
+		if cell:
+			cell.flip_h = true
---
+++ menu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_play_btn_pressed():
+	get_tree().change_scene_to_file("res://lvl_1.tscn")
+
+
+func _on_opt_btn_pressed():
+	pass # Replace with function body.
